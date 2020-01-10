e518ec6c13f711524d578de7ec4b544ab53468aa
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-09 21:30:46 -0300

    Projeto criado

---
+++ src/main/java/com/thiagopolli/mastersworks/MastersworksApplication.java
@@ -0,0 +1,13 @@
+package com.thiagopolli.mastersworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastersworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastersworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagopolli/mastersworks/MastersworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagopolli.mastersworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastersworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
