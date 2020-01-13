44b3633fd6e0594ceaf9de74b87713f81195504f
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-12 22:44:29 -0300

    validação sintatica com Bean Validation

--- src/main/java/com/thiagopolli/mastersworks/dto/CategoriaDTO.java
+++ src/main/java/com/thiagopolli/mastersworks/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.thiagopolli.mastersworks.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.thiagopolli.mastersworks.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
@@ -10,6 +14,9 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigaório")
+	@Length(min = 5, max = 80 , message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
