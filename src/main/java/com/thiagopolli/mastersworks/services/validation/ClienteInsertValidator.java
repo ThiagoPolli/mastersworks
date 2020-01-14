6f27b8b731489566a62e44ff2ef8048984b863a5
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-14 19:45:44 -0300

    validacao email, email ja existente ao cadastrar cliente

--- src/main/java/com/thiagopolli/mastersworks/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/thiagopolli/mastersworks/services/validation/ClienteInsertValidator.java
@@ -7,12 +7,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.thiagopolli.mastersworks.domain.Cliente;
 import com.thiagopolli.mastersworks.domain.enums.Tipocliente;
 import com.thiagopolli.mastersworks.dto.ClienteNewDTO;
+import com.thiagopolli.mastersworks.repositories.ClienteRepository;
 import com.thiagopolli.mastersworks.resources.exception.FildMessage;
 import com.thiagopolli.mastersworks.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo ;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if (objDto.getTipo().equals(Tipocliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getcpfouCnpj())) {
 			list.addAll(Arrays.asList(new FildMessage("cpfouCnpj", "CNPJ inválido")));
+		}
+		
+		//logica para testar se o email ja existe 
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.addAll(Arrays.asList(new FildMessage("email", "Email Já existe")));	
 		}
 
 		
