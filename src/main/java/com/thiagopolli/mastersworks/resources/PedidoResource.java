2b713ff0773bf7b0f68305faa16bc79434f39720
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-16 22:36:57 -0300

    Crinado Profile de test

--- src/main/java/com/thiagopolli/mastersworks/resources/PedidoResource.java
+++ src/main/java/com/thiagopolli/mastersworks/resources/PedidoResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.thiagopolli.mastersworks.domain.Categoria;
 import com.thiagopolli.mastersworks.domain.Pedido;
-import com.thiagopolli.mastersworks.dto.CategoriaDTO;
 import com.thiagopolli.mastersworks.services.PedidoServices;
 
 @RestController
