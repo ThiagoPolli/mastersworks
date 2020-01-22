4ba2ac0accc95da8b630b067e21642d6bd277421
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-21 23:52:10 -0300

    Implementando autorizacao

--- src/main/java/com/thiagopolli/mastersworks/config/SecurityConfig.java
+++ src/main/java/com/thiagopolli/mastersworks/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.thiagopolli.mastersworks.security.JWTAuthenticationFilter;
+import com.thiagopolli.mastersworks.security.JWTAuthorizationFilter;
 import com.thiagopolli.mastersworks.security.JWTUtil;
 
 @Configuration
@@ -58,7 +59,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		
+		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(),jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(),jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
