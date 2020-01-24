80031af1f2a2ed08c70491d628b24e7a7928b0d2
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-23 23:12:21 -0300

    Salvando arquivo no S3 Amazon

--- src/main/java/com/thiagopolli/mastersworks/MastersworksApplication.java
+++ src/main/java/com/thiagopolli/mastersworks/MastersworksApplication.java
@@ -1,12 +1,17 @@
 package com.thiagopolli.mastersworks;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.thiagopolli.mastersworks.services.S3Service;
+
 @SpringBootApplication
 public class MastersworksApplication implements CommandLineRunner {
 	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MastersworksApplication.class, args);
@@ -14,7 +19,7 @@ public class MastersworksApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("D:\\temp\\fotos\\lam.jpg");
 	}
 
 }
